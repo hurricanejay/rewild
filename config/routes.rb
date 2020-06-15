b2cdc86c3dee546ebeb698dd4717edcc7b7d4fbd
flatironschool <danliu277@gmail.com>
2020-06-15 15:43:33 -0400

    Create controller for plant and user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/plants', to: 'plants#index'
+  get '/plants/:id', to: 'plants#show'
+
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+  post '/users', to: 'users#create'
 end
