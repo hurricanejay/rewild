e8c3e1dbf2dda32d670ff92b5f30ee5fd20ab9d0
flatironschool <danliu277@gmail.com>
2020-06-22 17:04:31 -0400

    Add top 3 plant route

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -8,4 +8,9 @@ class PlantsController < ApplicationController
         plant = Plant.find(params[:id])
         render json: plant
     end
+
+    def top_three
+        plants = Hash[UserPlant.group(:plant_id).count.sort_by{|k, v| -v}.first(3)].keys.map{|id| Plant.find(id)}
+        render json: plants
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/plants', to: 'plants#index'
+  get '/plants/top_three', to: 'plants#top_three'
   get '/plants/:id', to: 'plants#show'
 
   get '/users', to: 'users#index'
