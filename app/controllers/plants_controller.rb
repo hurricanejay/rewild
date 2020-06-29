5a0d7f2fa9f0f14277cfdcbff940bd3ae2ff2c71
flatironschool <danliu277@gmail.com>
2020-06-29 14:32:52 -0400

    Update

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -10,6 +10,7 @@ class PlantsController < ApplicationController
     end
 
     def top_three
+        # Group UserPlant by plant_id, count them, sort by count/ value, get top 3, convert to hash, get keys, map id to Plant
         plants = Hash[UserPlant.group(:plant_id).count.sort_by{|k, v| -v}.first(3)].keys.map{|id| Plant.find(id)}
         render json: plants
     end
